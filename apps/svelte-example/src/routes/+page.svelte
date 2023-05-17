7056c9065a505f33f5074f9854788d9da2f69091
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-05-17 16:39:29 +0200

    clean up

--- apps/svelte-example/src/routes/+page.svelte
+++ apps/svelte-example/src/routes/+page.svelte
@@ -5,23 +5,26 @@
 	const serverFeatureFlag = useFeatureFlag('test3');
 </script>
 
-<body class="bg-red-100">
-	<h1>
-		AB testing with buckets In this demo we use cookies to assign a bucket with the variant to show.
-	</h1>
-	<p>Visit one of the pages below and a bucket will be assigned to you.</p>
-	<ul>
-		<li>
-			<a href="/home"> /home </a>
-		</li>
-		<li>
-			<a href="/marketing"> /marketing </a>
-		</li>
-	</ul>
-	{#if $clientFeatureFlag}
-		<p>If you see this text the client flag is enabled</p>
-	{/if}
-	{#if $serverFeatureFlag}
-		If you see this text the server side flag is enabled
-	{/if}
-</body>
+<h1>
+	AB testing with buckets In this demo we use cookies to assign a bucket with the variant to show.
+</h1>
+<p>Visit one of the pages below and a bucket will be assigned to you.</p>
+<ul>
+	<li>
+		<a href="/home"> /home </a>
+	</li>
+	<li>
+		<a href="/marketing"> /marketing </a>
+	</li>
+</ul>
+{#if $clientFeatureFlag}
+	<p>If you see this text the client flag is enabled</p>
+{/if}
+{#if $serverFeatureFlag}
+	If you see this text the server side flag is enabled
+{/if}
+
+<style lang="postcss">
+	:global(html) {
+	}
+</style>
