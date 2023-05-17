c1adae22917ffa6a2a3644b29f4cb2e87368f9ed
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-05-17 14:01:03 +0200

    fix tests

---
+++ packages/svelte/src/tests/pages/+test.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { abby } from '../abby';
+	export let data: { __abby__data: any; __abby_cookie: any };
+	const { useFeatureFlag, AbbyProvider } = abby;
+	const flag1 = useFeatureFlag('flag1');
+	const flag2 = useFeatureFlag('flag2');
+</script>
+
+<body>
+	<AbbyProvider abby={abby.__abby__} {data}>
+		{#if $flag1}
+			<div id="flag1Enabled">my super secret feature 1</div>
+		{/if}
+
+		{#if $flag2}
+			<div id="flag2Enabled">my super secret feature 2</div>
+		{/if}
+	</AbbyProvider>
+</body>
