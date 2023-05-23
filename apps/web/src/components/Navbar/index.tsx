0069bfb2714ffcd5985dd10a1abc47c97e71125e
Tim Raderschad <tim.raderschad@gmail.com>
2023-05-23 16:25:34 +0200

    feat(web): add github stars to navbar

--- apps/web/src/components/Navbar/index.tsx
+++ apps/web/src/components/Navbar/index.tsx
@@ -1,12 +1,13 @@
 import { Menu, Transition } from "@headlessui/react";
+import { DOCS_URL } from "@tryabby/core";
 import clsx from "clsx";
 import Logo from "components/Logo";
 import { cn } from "lib/utils";
+import { Star } from "lucide-react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { Fragment } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { DOCS_URL } from "@tryabby/core";
 import { trpc } from "utils/trpc";
 
 const NAV_ITEMS = [
@@ -30,6 +31,8 @@ const NAV_ITEMS = [
 
 function MobileNav({ isInverted }: { isInverted?: boolean }) {
   const { data, status } = useSession();
+  const { data: starsCount, isLoading: isStarsLoading } =
+    trpc.misc.getStars.useQuery();
   return (
     <Menu>
       <Menu.Button
@@ -49,7 +52,7 @@ function MobileNav({ isInverted }: { isInverted?: boolean }) {
       >
         <Menu.Items
           className={cn(
-            "absolute top-[80px] z-10 flex w-[calc(100%-3rem)] flex-col space-y-4 rounded-lg p-4 shadow-xl",
+            "absolute right-12 top-[80px] z-10 flex w-[calc(100%-6rem)] flex-col space-y-4 rounded-lg p-4 shadow-xl",
             isInverted ? "bg-zinc-800" : "bg-white"
           )}
         >
@@ -65,6 +68,16 @@ function MobileNav({ isInverted }: { isInverted?: boolean }) {
               )}
             </Menu.Item>
           ))}
+          <Menu.Item>
+            <NavItem
+              href="https://github.com/tryabby/abby"
+              className="mr-0"
+              isInverted={isInverted}
+            >
+              <Star className="h-5 w-5" />
+              <span>{isStarsLoading ? "0" : `${starsCount}`}</span>
+            </NavItem>
+          </Menu.Item>
           <Menu.Item>
             {status === "authenticated" ? (
               <NavItem
@@ -137,6 +150,8 @@ function NavItem({
 
 export function Navbar({ isInverted }: { isInverted?: boolean }) {
   const { data, isLoading, isError } = trpc.user.getUserData.useQuery();
+  const { data: starsCount, isLoading: isStarsLoading } =
+    trpc.misc.getStars.useQuery();
   return (
     <nav className="container relative flex items-center justify-between px-6 py-6 md:px-16">
       <div className="flex items-center space-x-4">
@@ -149,25 +164,34 @@ export function Navbar({ isInverted }: { isInverted?: boolean }) {
           </NavItem>
         ))}
       </div>
-      {!isLoading && !isError ? (
-        <NavItem
-          href={`/projects/${data.projects[0]?.id}`}
-          isProminent
-          isInverted={isInverted}
-          className="hidden lg:flex"
+      <div className="flex items-center space-x-3">
+        <Link
+          href="https://github.com/tryabby/abby"
+          className="hidden h-full items-center justify-center space-x-2 rounded-lg border-2 border-pink-600 px-4 py-2 font-medium transition-colors ease-in-out hover:bg-pink-300/20 lg:flex"
         >
-          Dashboard
-        </NavItem>
-      ) : (
-        <NavItem
-          isInverted={isInverted}
-          isProminent
-          href="/login"
-          className="hidden lg:flex"
-        >
-          Log In
-        </NavItem>
-      )}
+          <Star className="h-5 w-5" />
+          <span>{isStarsLoading ? "0" : `${starsCount}`}</span>
+        </Link>
+        {!isLoading && !isError ? (
+          <NavItem
+            href={`/projects/${data.projects[0]?.id}`}
+            isProminent
+            isInverted={isInverted}
+            className="hidden lg:flex"
+          >
+            Dashboard
+          </NavItem>
+        ) : (
+          <NavItem
+            isInverted={isInverted}
+            isProminent
+            href="/login"
+            className="hidden lg:flex"
+          >
+            Log In
+          </NavItem>
+        )}
+      </div>
       <MobileNav isInverted={isInverted} />
     </nav>
   );
