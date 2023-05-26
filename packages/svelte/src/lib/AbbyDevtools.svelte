ac711702327d77b9b68518cb9262fbf6d4e2955c
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-05-26 15:43:00 +0200

    dont show devTools in production

--- packages/svelte/src/lib/AbbyDevtools.svelte
+++ packages/svelte/src/lib/AbbyDevtools.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-  import { DevtoolsFactory, type AbbyDevtoolProps } from "@tryabby/devtools";
-  import { onMount, tick } from "svelte";
-  import type { Abby } from "@tryabby/core";
-  export let props: Omit<AbbyDevtoolProps, "abby">;
-  export let abby: Abby<any, any, any>;
-  onMount(async () => {
-    await tick();
-    const factory = new DevtoolsFactory();
-    factory.create({ ...props, abby });
-  });
+	import { DevtoolsFactory, type AbbyDevtoolProps } from '@tryabby/devtools';
+	import { onMount, tick } from 'svelte';
+	import type { Abby } from '@tryabby/core';
+	export let props: Omit<AbbyDevtoolProps, 'abby'>;
+	export let abby: Abby<any, any, any>;
+	onMount(async () => {
+		await tick();
+		if (!props?.dangerouslyForceShow && process.env.NODE_ENV !== 'development') {
+			return;
+		}
+		const factory = new DevtoolsFactory();
+		factory.create({ ...props, abby });
+	});
 </script>
