83de4ffee914fdd61134cd530b5ec42e64ec512a
Tim <82676248+Tim-53@users.noreply.github.com>
2023-05-31 21:30:03 +0200

    switch to nodemon

--- packages/nodejs/src/index.ts
+++ packages/nodejs/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express"
-import { useFeatureFlag } from "./src/abby";
+// import { useFeatureFlag } from "./lib/abby.ts";
 
+console.log("hi")
 const app = express();
 const port = 3000;
 
 app.get('/', async (req, res) => {
-    if (await useFeatureFlag("lol")) {
+    if (1) {
         res.send("not enabled")
     }
 });
--- packages/nodejs/src/lib/createAbby.ts
+++ packages/nodejs/src/lib/createAbby.ts
@@ -1,6 +1,5 @@
 import { ABConfig, Abby, AbbyConfig, HttpService, ABBY_BASE_URL } from "@tryabby/core";
 import { F } from "ts-toolbelt"
-// import fetch from 'node-fetch';
 
 export function createAbby<
     FlagName extends string,
@@ -14,14 +13,9 @@ export function createAbby<
 
     const useFeatureFlag = async <F extends NonNullable<ConfigType["flags"]>[number]>(
         name: F
-    ) => {
-        // const res = await fetch(
-        //     `${ABBY_BASE_URL}api/dashboard/${abbyConfig.projectId}/data`
-        // );
+    ) => {   
         console.log(2)
         return true
-        // await abby.loadProjectData()
-        // return abby.getFeatureFlag(name);
     };
 
     return { useFeatureFlag }
