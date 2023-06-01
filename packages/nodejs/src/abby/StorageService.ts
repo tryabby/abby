d6ceca7a0eb4c0296d2ab72d0943599d70d5e332
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-06-01 17:25:47 +0200

    add cookieMiddleware
    cookie middleware working

--- packages/nodejs/src/abby/StorageService.ts
+++ packages/nodejs/src/abby/StorageService.ts
@@ -5,20 +5,18 @@ import { getResponse } from "./responseContext.ts";
 
 class ABStorageService implements IStorageService {
     get(projectId: string, key: string): string | null {
-        return null;
-        // const req = getRequest();
-        // if (!req) return null;
-        // const cookieMap = parseCookies(req);
-        // const cookieKey = getABStorageKey(projectId, key);
-        // const cookieValue = cookieMap.get(cookieKey)
-        // return cookieValue ?? null;
+        const req = getRequest();
+        if (!req) return null;
+        const cookieMap = parseCookies(req);
+        const cookieKey = getABStorageKey(projectId, key);
+        const cookieValue = cookieMap.get(cookieKey)
+        return cookieValue ?? null;
     }
     set(projectId: string, key: string, value: string): void {
         const response = getResponse();
-        console.log("resp:", response?.cookie)
-        // if (!response) throw new Error("no response object")
-        // console.log("set cookie")
-        // response.cookie(key, value)
+        if (!response) throw new Error("no response object")
+        const cookieKey = getABStorageKey(projectId, key);
+        response.cookie(cookieKey, value)
     }
     remove(projectId: string, key: string): void {
         throw new Error("Method not implemented.");
