f46c9ccd4677f86646a2676f23e90541b07ec772
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-06-09 17:17:46 +0200

    add: test WIP

--- packages/nodejs/src/express/abbyMiddleware.ts
+++ packages/nodejs/src/express/abbyMiddleware.ts
@@ -1,24 +1,33 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 //TODO fix eslint
-import { getFeatureFlagValue, getABTestValue } from "../abby/abby.ts";
-import { NextFunction, Request, Response } from "express";
-import { setRequest } from "../abby/contexts/requestContext.ts";
-import { setResponse } from "../abby/contexts/responseContext.ts";
+import { getFeatureFlagValue, getABTestValue } from '../abby/abby.ts';
+import { NextFunction, Request, Response } from 'express';
+import { setRequest } from '../abby/contexts/requestContext.ts';
+import { setResponse } from '../abby/contexts/responseContext.ts';
 
-export const featureFlagMiddleware = async (req: Request, res: Response, name: string, next: NextFunction) => { //Todo fix types
-    const flagValue = await getFeatureFlagValue(name as any); //type?
-    console.log("hi from middleware", name, flagValue)
-    if (!flagValue) {
-        res.sendStatus(403);
-        return;
-    }
-    next();
-}
+//TODO make types narrow like in createAbby
+//TODO make abby import configurable
+
+export const featureFlagMiddleware = async (
+	req: Request,
+	res: Response,
+	name: string,
+	next: NextFunction
+) => {
+	//Todo fix types
+	const flagValue = getFeatureFlagValue(name as any); //type?
+	console.log('hi from middleware', name, flagValue);
+	if (!flagValue) {
+		res.sendStatus(403);
+		return;
+	}
+	next();
+};
 
 export const AbTestMiddleware = (req: Request, res: Response, name: string, next: NextFunction) => {
-    setRequest(req)
-    setResponse(res)
-    const value = getABTestValue(req, name as any);
-    console.log(value);
-    next();
-}
+	setRequest(req);
+	setResponse(res);
+	const value = getABTestValue(req, name as any);
+	console.log(value);
+	next();
+};
--- packages/nodejs/src/tests/abby.test.ts
+++ packages/nodejs/src/tests/abby.test.ts
@@ -1,9 +1,26 @@
-import { expect, test, describe } from "vitest"
+import { expect, test, describe } from 'vitest';
+import { createAbby } from '../abby/createAbby.ts';
 
-describe("it works", () => {
-    test("test", () => {
-        console.log("test running")
-        expect(true, "ja").toBeTruthy();
-    })
-}
-)
+describe('it works', () => {
+	test('getFeatureFlagValue working', async () => {
+		const { getABTestValue, getFeatureFlagValue } = await createAbby({
+			projectId: '',
+			tests: {},
+			flags: ['flag1', 'flag2']
+		});
+
+		console.log(await getFeatureFlagValue('flag1'));
+
+		expect(await getFeatureFlagValue('flag1')).toBeTruthy();
+		expect(await getFeatureFlagValue('flag2')).toBeFalsy();
+	});
+
+	test('getABTestValue working', async () => {
+		const { getABTestValue, getFeatureFlagValue } = await createAbby({
+			projectId: '',
+			tests: {
+				'New Test1': {}
+			}
+		});
+	});
+});
