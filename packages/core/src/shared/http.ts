d0a85aea8979619909f828ac4edc1be6e7adfdf5
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-06-13 13:33:15 +0200

    tests wip

--- packages/core/src/shared/http.ts
+++ packages/core/src/shared/http.ts
@@ -1,67 +1,71 @@
-import { ABBY_BASE_URL } from "./constants";
-import type { AbbyEventType, AbbyEvent, AbbyDataResponse } from "./index";
+import { ABBY_BASE_URL } from './constants';
+import type { AbbyEventType, AbbyEvent, AbbyDataResponse } from './index';
 
-function fetchData(url: RequestInfo | URL, init?: RequestInit) {
-  if (typeof window === 'undefined') {
-    // Running in Node.js
-    const fetch = require('node-fetch');
-    return fetch(url)
-  } else {
-    // Running in a browser
-    return fetch(url);
-  }
-}
+//
+// function fetchData(url: RequestInfo | URL, init?: RequestInit) {
+// if (typeof window === 'undefined') {
+// Running in Node.js
+// const fetch = require('node-fetch');
+// return fetch(url);
+// } else {
+// Running in a browser
+// return fetch(url);
+// }
+// }
+
 export abstract class HttpService {
-  static async getProjectData({
-    projectId,
-    environment,
-    url,
-  }: {
-    projectId: string;
-    environment?: string;
-    url?: string;
-  }) {
-    try {
-      const res = await fetchData(
-        `${url ?? ABBY_BASE_URL}api/dashboard/${projectId}/data${environment ? `?environment=${environment}` : ""
-        }`
-      );
+	static async getProjectData({
+		projectId,
+		environment,
+		url
+	}: {
+		projectId: string;
+		environment?: string;
+		url?: string;
+	}) {
+		try {
+			console.log(
+				`${url ?? ABBY_BASE_URL}api/dashboard/${projectId}/data${
+					environment ? `?environment=${environment}` : ''
+				}`
+			);
+			const res = await fetch(
+				`${url ?? ABBY_BASE_URL}api/dashboard/${projectId}/data${
+					environment ? `?environment=${environment}` : ''
+				}`
+			);
 
-      if (!res.ok) return null;
-      const data = (await res.json()) as AbbyDataResponse;
-      return data;
-    } catch (err) {
-      console.error(
-        "[ABBY]: failed to load project data, falling back to defaults"
-      );
-      return null;
-    }
-  }
+			if (!res.ok) return null;
+			const data = (await res.json()) as AbbyDataResponse;
+			return data;
+		} catch (err) {
+			console.error('[ABBY]: failed to load project data, falling back to defaults');
+			console.log(err);
+			return null;
+		}
+	}
 
-  static sendData({
-    url,
-    type,
-    data,
-  }: {
-    url?: string;
-    type: AbbyEventType;
-    data: Omit<AbbyEvent, "type">;
-  }) {
-    if (
-      typeof window === "undefined" ||
-      window.location.hostname === "localhost"
-    ) {
-      // don't send data in development
-      return;
-    }
-    return fetchData(`${url ?? ABBY_BASE_URL}api/data`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ type, ...data }),
-      // catch error and forget about them for now
-      // TODO: add error debugging
-    }).catch(() => { });
-  }
+	static sendData({
+		url,
+		type,
+		data
+	}: {
+		url?: string;
+		type: AbbyEventType;
+		data: Omit<AbbyEvent, 'type'>;
+	}) {
+		if (typeof window === 'undefined' || window.location.hostname === 'localhost') {
+			// don't send data in development
+			return;
+		}
+		return fetch(`${url ?? ABBY_BASE_URL}api/data`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ type, ...data })
+			// catch error and forget about them for now
+			// TODO: add error debugging
+		}).catch(() => {});
+	}
 }
