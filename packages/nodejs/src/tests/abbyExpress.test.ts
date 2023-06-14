69ae44608eca502ec6666d68f42deaf565321937
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-06-14 18:11:46 +0200

    fix build

--- packages/nodejs/src/tests/abbyExpress.test.ts
+++ packages/nodejs/src/tests/abbyExpress.test.ts
@@ -1,44 +1,46 @@
 import { expect, test, describe, beforeEach } from 'vitest';
-import { createAbby } from '../abby/createAbby.ts';
 import express, { response } from 'express';
-import { AbTestMiddleware, featureFlagMiddleware } from '../express/abbyMiddlewareFactory.ts';
 import request from 'supertest';
+import { abbyMiddlewareFactory } from '../express/abbyMiddlewareFactory.ts';
 
-describe.skip('express middleware working', () => {
+describe('express middleware working', () => {
 	let app: express.Application;
 
-	beforeEach(() => {
+	beforeEach(async () => {
+		const { AbTestMiddleware, featureFlagMiddleware } = await abbyMiddlewareFactory({
+			abbyConfig: {
+				projectId: 'clfn3hs1t0002kx08x3kidi80',
+				currentEnvironment: process.env.NODE_ENV,
+				tests: {
+					'New Test3': {
+						variants: ['A', 'B']
+					}
+				},
+				flags: ['lol', 'test3', 'testAbby'],
+				flagCacheConfig: {
+					refetchFlags: true,
+					timeToLive: 1
+				}
+			}
+		});
 		app = express();
 		// Add any other middlewares or routes necessary for your test
+		app.use('/featureflagTest', (req, res, next) => featureFlagMiddleware('test2', req, res, next));
+		app.get('/', (req, res) => {
+			res.send('hiadsa');
+		});
 	});
 
 	test('abTestMiddleware working', async () => {
-		const result = true;
 		//test cookie retrieval
-		// app.use('/', (req, res, next) => AbTestMiddleware(req, res, '', next));
-		app.get('/', (req, res) => {
-			res.status(200);
-			res.send('hi');
-		});
 
 		const res = await request(app).get('/');
-		console.log(res.statusCode);
+
+		console.log(res.text);
 		//check if cookie is set
-		expect(result).toBeFalsy();
 	});
 
 	test.skip('featureFlag Middleware working', async () => {
-		const result = true;
-		app.use('/', (req, res, next) => AbTestMiddleware(req, res, '', next));
-		app.get('/', (req, res) => {
-			res.status(200);
-			res.send('hi');
-		});
-
-		const res = await request(app).get('/');
-		console.log(res.statusCode);
-
-		expect(result).toBeFalsy();
 		//check if feature flag value is respected
 	});
 });
