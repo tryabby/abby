d9dd7784263213c84eee15295258a6f12430b2fa
Tim <82676248+Tim-53@users.noreply.github.com>
2023-06-14 21:49:55 +0200

    add express test middleware setting cookie

--- packages/nodejs/src/abby/createAbby.ts
+++ packages/nodejs/src/abby/createAbby.ts
@@ -1,44 +1,48 @@
-import { ABConfig, Abby, AbbyConfig } from '@tryabby/core';
-import { F } from 'ts-toolbelt';
-import { Request } from 'express';
-import { TestStorageService } from './StorageService.ts';
+import { ABConfig, Abby, AbbyConfig } from "@tryabby/core";
+import { F } from "ts-toolbelt";
+import { Request } from "express";
+import { TestStorageService } from "./StorageService.ts";
 
 export async function createAbby<
-	FlagName extends string,
-	TestName extends string,
-	Tests extends Record<TestName, ABConfig>,
-	ConfigType extends AbbyConfig<FlagName, Tests> = AbbyConfig<FlagName, Tests>
+  FlagName extends string,
+  TestName extends string,
+  Tests extends Record<TestName, ABConfig>,
+  ConfigType extends AbbyConfig<FlagName, Tests> = AbbyConfig<FlagName, Tests>
 >(abbyConfig: F.Narrow<AbbyConfig<FlagName, Tests>>) {
-	const abby = new Abby<FlagName, TestName, Tests>(abbyConfig, {
-		get: (key: string) => {
-			return TestStorageService.get(abbyConfig.projectId, key);
-		},
-		set: (key: string, value: any) => {
-			TestStorageService.set(abbyConfig.projectId, key, value);
-		}
-	});
+  const abby = new Abby<FlagName, TestName, Tests>(abbyConfig, {
+    get: (key: string) => {
+      return TestStorageService.get(abbyConfig.projectId, key);
+    },
+    set: (key: string, value: any) => {
+      TestStorageService.set(abbyConfig.projectId, key, value);
+    },
+  });
 
-	//load data and initialise the abby Object
-	await abby.loadProjectData();
+  //load data and initialise the abby Object
+  await abby.loadProjectData();
 
-	const config = abbyConfig as unknown as ConfigType;
+  const config = abbyConfig as unknown as ConfigType;
 
-	/**
-	 * @param name Name of the test that the variant should be retrieved for
-	 * @returns Value of the currently selected variant
-	 */
-	const getABTestValue = <T extends keyof Tests>(name: T) => {
-		const value = abby.getTestVariant(name);
-		return value;
-	};
-	/**
-	 *
-	 * @param name Name of the feature flag
-	 * @returns Value of the feature flag
-	 */
-	const getFeatureFlagValue = <F extends NonNullable<ConfigType['flags']>[number]>(name: F) => {
-		return abby.getFeatureFlag(name);
-	};
+  /**
+   * @param name Name of the test that the variant should be retrieved for
+   * @returns Value of the currently selected variant
+   */
+  const getABTestValue = <T extends keyof Tests>(name: T) => {
+    const value = abby.getTestVariant(name);
+    return value;
+  };
+  /**
+   *
+   * @param name Name of the feature flag
+   * @returns Value of the feature flag
+   */
+  const getFeatureFlagValue = <
+    F extends NonNullable<ConfigType["flags"]>[number]
+  >(
+    name: F
+  ) => {
+    return abby.getFeatureFlag(name);
+  };
 
-	return { getFeatureFlagValue, getABTestValue };
+  return { getFeatureFlagValue, getABTestValue };
 }
--- packages/nodejs/src/tests/abbyExpress.test.ts
+++ packages/nodejs/src/tests/abbyExpress.test.ts
@@ -1,46 +1,57 @@
-import { expect, test, describe, beforeEach } from 'vitest';
-import express, { response } from 'express';
-import request from 'supertest';
-import { abbyMiddlewareFactory } from '../express/abbyMiddlewareFactory.ts';
+import { expect, test, describe, beforeEach } from "vitest";
+import express, { response } from "express";
+import request, { Response, Request } from "supertest";
+import { abbyMiddlewareFactory } from "../express/abbyMiddlewareFactory.ts";
 
-describe('express middleware working', () => {
-	let app: express.Application;
+describe("express middleware working", () => {
+  let app: express.Application;
 
-	beforeEach(async () => {
-		const { AbTestMiddleware, featureFlagMiddleware } = await abbyMiddlewareFactory({
-			abbyConfig: {
-				projectId: 'clfn3hs1t0002kx08x3kidi80',
-				currentEnvironment: process.env.NODE_ENV,
-				tests: {
-					'New Test3': {
-						variants: ['A', 'B']
-					}
-				},
-				flags: ['lol', 'test3', 'testAbby'],
-				flagCacheConfig: {
-					refetchFlags: true,
-					timeToLive: 1
-				}
-			}
-		});
-		app = express();
-		// Add any other middlewares or routes necessary for your test
-		app.use('/featureflagTest', (req, res, next) => featureFlagMiddleware('test2', req, res, next));
-		app.get('/', (req, res) => {
-			res.send('hiadsa');
-		});
-	});
+  beforeEach(async () => {
+    const { AbTestMiddleware, featureFlagMiddleware } =
+      await abbyMiddlewareFactory({
+        abbyConfig: {
+          projectId: "123",
+          currentEnvironment: process.env.NODE_ENV,
+          tests: {
+            test: {
+              variants: ["A", "B", "C", "D"],
+            },
+            test2: {
+              variants: ["A", "B"],
+            },
+          },
+          flags: ["flag1", "flag2"],
+        },
+      });
+    app = express();
+    // Add any other middlewares or routes necessary for your test
+    app.use("/featureFlagEnabled", (req, res, next) =>
+      featureFlagMiddleware("flag1", req, res, next)
+    );
+    app.use("/featureFlagDisabled", (req, res, next) =>
+      featureFlagMiddleware("flag2", req, res, next)
+    );
+    app.use("/cookieNeedToBeSet", (req, res, next) =>
+      AbTestMiddleware("test2", req, res, next)
+    );
+    app.get("/cookieSet", (req, res) => {
+      res.send("hiadsa");
+    });
+  });
 
-	test('abTestMiddleware working', async () => {
-		//test cookie retrieval
+  test.skip("abTestMiddleware respects the set cookie", async () => {
+    //test cookie retrieval
 
-		const res = await request(app).get('/');
+    const res = await request(app).get("/");
+  });
 
-		console.log(res.text);
-		//check if cookie is set
-	});
+  test("abTestMiddleware sets the right cookie", async () => {
+    const res = await request(app).get("/cookieNeedToBeSet");
+    const cookies = res.headers["set-cookie"]; //res headers is any so need to be carefull
+    expect(cookies[0]).toBe("__abby__ab__123_test2=A; Path=/");
+  });
 
-	test.skip('featureFlag Middleware working', async () => {
-		//check if feature flag value is respected
-	});
+  test.skip("featureFlag Middleware working", async () => {
+    //check if feature flag value is respected
+  });
 });
