4db3e43c49119b0bd23e432bc88bcb62f2708399
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-06-15 15:05:50 +0200

    remove node-fetch from corePackage

--- packages/core/tests/setup.ts
+++ packages/core/tests/setup.ts
@@ -2,11 +2,6 @@ import { expect, afterEach } from 'vitest';
 
 import { server } from './mocks/server';
 
-// import fetch from 'node-fetch';
-
-// /// @ts-ignore
-// global.fetch = fetch;
-
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
 // Reset any request handlers that we may add during the tests,
