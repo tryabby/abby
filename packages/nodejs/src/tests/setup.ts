498315f366db145c52d1aeb3dafceb427ffe99ad
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-06-15 14:03:04 +0200

    disabled msw warnings for calls to express

--- packages/nodejs/src/tests/setup.ts
+++ packages/nodejs/src/tests/setup.ts
@@ -1,13 +1,25 @@
-import { expect, afterEach } from 'vitest';
+import { expect, afterEach } from "vitest";
 
-import { server } from './mocks/server.ts';
-import fetch from 'node-fetch';
+import { server } from "./mocks/server.ts";
+import fetch from "node-fetch";
 
 /// @ts-ignore
 global.fetch = fetch;
 
 // Establish API mocking before all tests.
-beforeAll(() => server.listen());
+beforeAll(() =>
+  server.listen({
+    onUnhandledRequest(req) {
+      //requests to express should not be intercepted and there should be no warning
+      const excludedRoutes = ["/cookie", "/featureFlag"];
+      const routeIsExcluded = excludedRoutes.some((route) => req.url.pathname.includes(route));
+      if (routeIsExcluded) {
+        return;
+      }
+      console.error("Found an unhandled %s request to %s", req.method, req.url.href);
+    },
+  })
+);
 // Reset any request handlers that we may add during the tests,
 // so they don't affect other tests.
 afterEach(() => server.resetHandlers());
