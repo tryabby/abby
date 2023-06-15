eec683908fe4ba39ccd0fb66a878c9b278c1d940
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-06-15 18:38:12 +0200

    fix angular tests wip

---
+++ packages/angular/karma-webpack.config.js
@@ -0,0 +1,6 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
+module.exports = {
+  // ...
+  plugins: [new NodePolyfillPlugin()],
+}
--- packages/angular/src/lib/abby.service.spec.ts
+++ packages/angular/src/lib/abby.service.spec.ts
@@ -1,263 +1,260 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AbbyService } from "./abby.service";
-import { AbbyModule } from "./abby.module";
+import { AbbyService } from './abby.service';
+import { AbbyModule } from './abby.module';
 
-import { Component, Injectable } from "@angular/core";
-import { TestStorageService } from "./StorageService";
-import { zip } from "rxjs";
-import { Routes } from "@angular/router";
-import { AbbyFlag } from "./flag.directive";
+import { Component, Injectable } from '@angular/core';
+import { TestStorageService } from './StorageService';
+import { zip } from 'rxjs';
+import { Routes } from '@angular/router';
+import { AbbyFlag } from './flag.directive';
 
 const mockConfig = {
-  projectId: "mock-project-id",
-  currentEnvironment: "test",
-  tests: {
-    test: {
-      variants: ["A", "B", "C", "D"],
-    },
-    test2: {
-      variants: ["A", "B"],
-    },
-    defaultTest: {
-      variants: ["A", "B"],
-    },
-  },
-  flags: ["flag1", "flag2", "overridedFlag1", "overridedFlag2", "defaultFlag"],
-  settings: {
-    flags: {
-      devOverrides: {
-        overridedFlag1: true,
-        overridedFlag2: false,
-      },
-    },
-  },
+	projectId: 'mock-project-id',
+	currentEnvironment: 'test',
+	tests: {
+		test: {
+			variants: ['A', 'B', 'C', 'D']
+		},
+		test2: {
+			variants: ['A', 'B']
+		},
+		defaultTest: {
+			variants: ['A', 'B']
+		}
+	},
+	flags: ['flag1', 'flag2', 'overridedFlag1', 'overridedFlag2', 'defaultFlag'],
+	settings: {
+		flags: {
+			devOverrides: {
+				overridedFlag1: true,
+				overridedFlag2: false
+			}
+		}
+	}
 };
 
 const mockedData = {
-  tests: [
-    {
-      name: "test",
-      weights: [1, 1, 1, 1],
-    },
-    {
-      name: "test2",
-      weights: [1, 0],
-    },
-  ],
-  flags: [
-    {
-      name: "flag1",
-      isEnabled: true,
-    },
-    {
-      name: "flag2",
-      isEnabled: false,
-    },
-    {
-      name: "overridedFlag1",
-      isEnabled: false,
-    },
-    {
-      name: "overridedFlag2",
-      isEnabled: true,
-    },
-  ],
+	tests: [
+		{
+			name: 'test',
+			weights: [1, 1, 1, 1]
+		},
+		{
+			name: 'test2',
+			weights: [1, 0]
+		}
+	],
+	flags: [
+		{
+			name: 'flag1',
+			isEnabled: true
+		},
+		{
+			name: 'flag2',
+			isEnabled: false
+		},
+		{
+			name: 'overridedFlag1',
+			isEnabled: false
+		},
+		{
+			name: 'overridedFlag2',
+			isEnabled: true
+		}
+	]
 };
 @Injectable({
-  providedIn: "root",
-  useExisting: AbbyService,
+	providedIn: 'root',
+	useExisting: AbbyService
 })
 export class Abby extends AbbyService<
-  (typeof mockConfig)["flags"][number],
-  keyof (typeof mockConfig)["tests"]
+	(typeof mockConfig)['flags'][number],
+	keyof (typeof mockConfig)['tests']
 > {}
 
-describe("AbbyService", () => {
-  let service: AbbyService;
-  let fixture: ComponentFixture<TestComponent>;
+describe('AbbyService', () => {
+	let service: AbbyService;
+	let fixture: ComponentFixture<TestComponent>;
 
-  @Component({
-    template: `
-      <div *featureFlag="'flag1'">
-        <h1>Flag 1</h1>
-      </div>
-      <div *featureFlag="'flag2'">
-        <h1>Flag 2</h1>
-      </div>
-      <div *abbyTest="{ testName: 'test2', variant: 'A' }">
-        <h2>A</h2>
-      </div>
-      <div *abbyTest="{ testName: 'test2', variant: 'B' }">
-        <h3>B</h3>
-      </div>
-    `,
-  })
-  class TestComponent {}
+	@Component({
+		template: `
+			<div *featureFlag="'flag1'">
+				<h1>Flag 1</h1>
+			</div>
+			<div *featureFlag="'flag2'">
+				<h1>Flag 2</h1>
+			</div>
+			<div *abbyTest="{ testName: 'test2', variant: 'A' }">
+				<h2>A</h2>
+			</div>
+			<div *abbyTest="{ testName: 'test2', variant: 'B' }">
+				<h3>B</h3>
+			</div>
+		`
+	})
+	class TestComponent {}
+	console.log('test');
+	beforeAll(async () => {
+		console.log('before all started');
+		const fetchSpy = spyOn(window, 'fetch');
 
-  beforeAll(async () => {
-    const fetchSpy = spyOn(window, "fetch");
+		const mockedResponse = new Response(JSON.stringify(mockedData), {
+			status: 200,
+			headers: { 'Content-type': 'application/json' }
+		});
 
-    const mockedResponse = new Response(JSON.stringify(mockedData), {
-      status: 200,
-      headers: { "Content-type": "application/json" },
-    });
+		fetchSpy.and.returnValue(Promise.resolve(mockedResponse));
 
-    fetchSpy.and.returnValue(Promise.resolve(mockedResponse));
+		await TestBed.configureTestingModule({
+			declarations: [TestComponent],
+			imports: [AbbyModule.forRoot(mockConfig)]
+		})
+			.compileComponents()
+			.then(() => {
+				fixture = TestBed.createComponent(TestComponent);
+				fixture.detectChanges();
+			});
+		service = TestBed.inject(AbbyService);
+		console.log('before all done');
+	});
 
-    await TestBed.configureTestingModule({
-      declarations: [TestComponent],
-      imports: [AbbyModule.forRoot(mockConfig)],
-    })
-      .compileComponents()
-      .then(() => {
-        fixture = TestBed.createComponent(TestComponent);
-        fixture.detectChanges();
-      });
-    service = TestBed.inject(AbbyService);
-  });
+	beforeEach(() => {});
 
-  beforeEach(() => {});
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 
-  it("should be created", () => {
-    expect(service).toBeTruthy();
-  });
+	it('gets the stored feature flag value using a function properly', () => {
+		service.getFeatureFlagValue('flag1').subscribe((value: boolean) => {
+			expect(value).toEqual(true);
+		});
 
-  it("gets the stored feature flag value using a function properly", () => {
-    service.getFeatureFlagValue("flag1").subscribe((value: boolean) => {
-      expect(value).toEqual(true);
-    });
+		service.getFeatureFlagValue('flag2').subscribe((value: boolean) => {
+			expect(value).toEqual(false);
+		});
+	});
 
-    service.getFeatureFlagValue("flag2").subscribe((value: boolean) => {
-      expect(value).toEqual(false);
-    });
-  });
+	it('returns the correct variant', () => {
+		service.getVariant('test2').subscribe((value: string) => {
+			expect(value).toEqual('A');
+		});
+	});
 
-  it("returns the correct variant", () => {
-    service.getVariant("test2").subscribe((value: string) => {
-      expect(value).toEqual("A");
-    });
-  });
+	it('should repect the default values for feature flags', () => {
+		service.getFeatureFlagValue('defaultFlag').subscribe((value: boolean) => {
+			expect(value).toEqual(false);
+		});
+	});
 
-  it("should repect the default values for feature flags", () => {
-    service.getFeatureFlagValue("defaultFlag").subscribe((value: boolean) => {
-      expect(value).toEqual(false);
-    });
-  });
+	it('should repect the default values for variants', () => {
+		service.getVariant('defaultTest').subscribe((value: string) => {
+			expect(['A', 'B']).toContain(value);
+		});
+	});
 
-  it("should repect the default values for variants", () => {
-    service.getVariant("defaultTest").subscribe((value: string) => {
-      expect(["A", "B"]).toContain(value);
-    });
-  });
+	it('uses the devOverrides', () => {
+		service.getFeatureFlagValue('overridedFlag1').subscribe((value: boolean) => {
+			expect(value).toEqual(true);
+		});
 
-  it("uses the devOverrides", () => {
-    service
-      .getFeatureFlagValue("overridedFlag1")
-      .subscribe((value: boolean) => {
-        expect(value).toEqual(true);
-      });
+		service.getFeatureFlagValue('overridedFlag2').subscribe((value: boolean) => {
+			expect(value).toEqual(false);
+		});
+	});
 
-    service
-      .getFeatureFlagValue("overridedFlag2")
-      .subscribe((value: boolean) => {
-        expect(value).toEqual(false);
-      });
-  });
+	it('should use the persistedValue', () => {
+		const persistedValue = 'A';
+		const variants = ['A', 'B', 'C', 'D'];
 
-  it("should use the persistedValue", () => {
-    const persistedValue = "A";
-    const variants = ["A", "B", "C", "D"];
+		const getSpy = spyOn(TestStorageService, 'get');
+		const setSpy = spyOn(TestStorageService, 'set');
 
-    const getSpy = spyOn(TestStorageService, "get");
-    const setSpy = spyOn(TestStorageService, "set");
+		getSpy.and.returnValue(persistedValue);
 
-    getSpy.and.returnValue(persistedValue);
+		service.getVariant('test').subscribe((value: string) => {
+			expect(setSpy).not.toHaveBeenCalled();
+			expect(getSpy).toHaveBeenCalled();
+			expect(value).toEqual(persistedValue);
+		});
+	});
 
-    service.getVariant("test").subscribe((value: string) => {
-      expect(setSpy).not.toHaveBeenCalled();
-      expect(getSpy).toHaveBeenCalled();
-      expect(value).toEqual(persistedValue);
-    });
-  });
+	it('should get the correct router variant', () => {
+		@Component({})
+		class ATestComponent {}
 
-  it("should get the correct router variant", () => {
-    @Component({})
-    class ATestComponent {}
+		@Component({})
+		class BTestComponent {}
 
-    @Component({})
-    class BTestComponent {}
+		@Component({})
+		class CTestComponent {}
 
-    @Component({})
-    class CTestComponent {}
+		@Component({})
+		class DTestComponent {}
 
-    @Component({})
-    class DTestComponent {}
+		@Component({})
+		class FlagComponent {}
 
-    @Component({})
-    class FlagComponent {}
+		let routes: Routes = [];
 
-    let routes: Routes = [];
+		zip(service.getVariant('test'), service.getFeatureFlagValue('flag1')).subscribe(
+			([angularTest, angularFlag]) => {
+				routes = [
+					service.getRouterVariant(angularTest, {
+						path: 'test',
+						outlet: 'test',
+						abbyVariants: {
+							A: {
+								title: 'TEST A',
+								component: ATestComponent
+							},
+							B: {
+								title: 'TEST B',
+								component: BTestComponent
+							},
+							C: {
+								title: 'TEST C',
+								component: CTestComponent
+							},
+							D: {
+								title: 'TEST D',
+								component: DTestComponent
+							}
+						}
+					}),
+					...(angularFlag
+						? [
+								{
+									path: 'flag',
+									title: 'Flag',
+									component: FlagComponent
+								}
+						  ]
+						: [])
+				];
 
-    zip(
-      service.getVariant("test"),
-      service.getFeatureFlagValue("flag1")
-    ).subscribe(([angularTest, angularFlag]) => {
-      routes = [
-        service.getRouterVariant(angularTest, {
-          path: "test",
-          outlet: "test",
-          abbyVariants: {
-            A: {
-              title: "TEST A",
-              component: ATestComponent,
-            },
-            B: {
-              title: "TEST B",
-              component: BTestComponent,
-            },
-            C: {
-              title: "TEST C",
-              component: CTestComponent,
-            },
-            D: {
-              title: "TEST D",
-              component: DTestComponent,
-            },
-          },
-        }),
-        ...(angularFlag
-          ? [
-              {
-                path: "flag",
-                title: "Flag",
-                component: FlagComponent,
-              },
-            ]
-          : []),
-      ];
+				expect(routes).not.toEqual([]);
+			}
+		);
+	});
 
-      expect(routes).not.toEqual([]);
-    });
-  });
+	it('returns the correct possible variant values', () => {
+		service.getVariants('test').subscribe((value: readonly string[]) => {
+			expect(value).toEqual(['A', 'B', 'C', 'D']);
+		});
+	});
 
-  it("returns the correct possible variant values", () => {
-    service.getVariants("test").subscribe((value: readonly string[]) => {
-      expect(value).toEqual(["A", "B", "C", "D"]);
-    });
-  });
+	it('directives should work', () => {
+		const compiled = fixture.nativeElement as HTMLElement;
+		let flagElement = compiled.querySelector('h1');
+		let testAElement = compiled.querySelector('h2');
+		let testBElement = compiled.querySelector('h3');
 
-  it("directives should work", () => {
-    const compiled = fixture.nativeElement as HTMLElement;
-    let flagElement = compiled.querySelector("h1");
-    let testAElement = compiled.querySelector("h2");
-    let testBElement = compiled.querySelector("h3");
-
-    if (flagElement) {
-      expect(flagElement.textContent).toEqual("Flag 1");
-    } else fail("querySelector is null");
-    if (testAElement) {
-      expect(testAElement.textContent).toEqual("A");
-    } else fail("querySelector is null");
-  });
+		if (flagElement) {
+			expect(flagElement.textContent).toEqual('Flag 1');
+		} else fail('querySelector is null');
+		if (testAElement) {
+			expect(testAElement.textContent).toEqual('A');
+		} else fail('querySelector is null');
+	});
 });
