dc44eb6e6705a8344eca99e1e66b6722f2d14d71
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-07-06 13:45:05 +0200

    add alll flags middleware

--- packages/nodejs/src/express/abbyMiddlewareFactory.ts
+++ packages/nodejs/src/express/abbyMiddlewareFactory.ts
@@ -5,8 +5,6 @@ import { AbbyConfig, ABConfig } from "@tryabby/core";
 import { createAbby } from "../abby/createAbby.ts";
 import { F } from "ts-toolbelt";
 
-type abbyMiddlewareConfig = {};
-
 export const abbyMiddlewareFactory = async <
   FlagName extends string,
   TestName extends string,
@@ -14,43 +12,64 @@ export const abbyMiddlewareFactory = async <
   ConfigType extends AbbyConfig<FlagName, Tests> = AbbyConfig<FlagName, Tests>
 >({
   abbyConfig,
-  config: abbyMiddlewarreConfig,
 }: {
   abbyConfig: F.Narrow<AbbyConfig<FlagName, Tests>>;
-  config?: abbyMiddlewareConfig;
 }) => {
-  const config = abbyConfig as unknown as ConfigType;
+  const configNarrowed = abbyConfig as unknown as ConfigType;
   const abby = await createAbby(abbyConfig);
 
   const featureFlagMiddleware = <F extends NonNullable<ConfigType["flags"]>[number]>(
     name: F,
     req: Request,
     res: Response,
-    next: NextFunction
+    next: NextFunction,
+    statusCode?: number,
+    errorMessage?: string
   ) => {
     const flagValue = abby.getFeatureFlagValue(name);
+
     if (!flagValue) {
-      res.status(403).json({});
+      res.status(statusCode ?? 403).json(errorMessage);
       return;
     }
     next();
   };
 
-  const AbTestMiddleware = <T extends keyof Tests>(
-    name: T,
+  const setRequestResponse = (req: Request, res: Response) => {
+    setRequest(req);
+    setResponse(res);
+  };
+
+  const extractTest = <T extends keyof Tests>(name: T) => {
+    const variant = abby.getABTestValue(name);
+    return { name, variant };
+  };
+
+  const allTestsMiddleWare = <T extends keyof Tests>(
     req: Request,
     res: Response,
     next: NextFunction
   ) => {
-    setRequest(req);
-    setResponse(res);
-    const value = abby.getABTestValue(name);
-    //make sure body is defined
-    req.body = req.body ?? {};
-    // appends test to body
-    req.body.ABBY = value;
+    if (configNarrowed.tests) {
+      setRequestResponse(req, res);
+      const allTests = Object.keys(configNarrowed.tests) as T[]; //TODO get type in a proper way
+      const testWithVariant = allTests.map((test) => {
+        return extractTest(test);
+      });
+    }
+
     next();
   };
 
-  return { featureFlagMiddleware, AbTestMiddleware };
+  const allFlagMiddleWare = <F extends NonNullable<ConfigType["flags"]>[number]>(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {};
+
+  const getVariant = <T extends keyof Tests>(name: T) => {
+    return abby.getABTestValue(name);
+  };
+
+  return { featureFlagMiddleware, allTestsMiddleWare, getVariant };
 };
--- packages/nodejs/src/index.ts
+++ packages/nodejs/src/index.ts
@@ -1,20 +1,40 @@
 import express from "express";
-// import { featureFlagMiddleware, AbTestMiddleware } from "./express/abbyMiddlewareFactory.ts";
 import { abbyMiddlewareFactory } from "./express/abbyMiddlewareFactory.ts";
 import { createAbby } from "./abby/createAbby.ts";
 
 const app = express();
 const port = 3000;
 
-// app.use("/", (req, res, next) => AbTestMiddleware(req, res, "New Test3", next));
-// app.use("/", (req: Request, res: Response, next: NextFunction) => featureFlagMiddleware(req, res, "testAbby", next))
+const { featureFlagMiddleware, allTestsMiddleWare, getVariant } = await abbyMiddlewareFactory({
+  abbyConfig: {
+    projectId: "clfn3hs1t0002kx08x3kidi80",
+    currentEnvironment: process.env.NODE_ENV,
+    tests: {
+      "New Test3": {
+        variants: ["A", "B"],
+      },
+      Test2: {
+        variants: ["A", "klein", "basic"],
+      },
+      "New Test6": {
+        variants: ["A"],
+      },
+    },
+    flags: ["lol", "test3", "testAbby"],
+    flagCacheConfig: {
+      refetchFlags: true,
+      timeToLive: 1,
+    },
+  },
+});
+
+app.use("/", (req, res, next) => allTestsMiddleWare(req, res, next));
+
+// app.use("/", (req, res, next) => featureFlagMiddleware("test3", req, res, next));
 
 app.get("/", async (req, res) => {
-  res.send("very nice content that needs to be protected");
+  const variant = getVariant("New Test3");
+  res.send(variant === "A" ? "very nice content that needs to be protected" : "vriant B");
 });
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
-
-const exportObj: any = { expressMiddlware: abbyMiddlewareFactory, NodeJsStandAlone: createAbby };
-
-export default exportObj;
