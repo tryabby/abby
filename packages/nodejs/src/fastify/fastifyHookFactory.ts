b5bf94ba7ed9239f297b4d417701c9fd34ea34f3
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-07-11 18:48:57 +0200

    update fastify factory

--- packages/nodejs/src/fastify/fastifyHookFactory.ts
+++ packages/nodejs/src/fastify/fastifyHookFactory.ts
@@ -1,13 +1,56 @@
+import { ABConfig, FlagValueString, AbbyConfig } from "@tryabby/core";
+import { F } from "ts-toolbelt";
+import { createAbby } from "../abby/createAbby";
 import { abby } from "./createAbby";
+import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from "fastify";
 
-export const fastifyHookFactory = () => {
-  const featureFlagHook = () => {};
+export const abbyFastifyFactory = <
+  FlagName extends string,
+  TestName extends string,
+  Tests extends Record<TestName, ABConfig>,
+  Flags extends Record<FlagName, FlagValueString> = Record<FlagName, FlagValueString>,
+  ConfigType extends AbbyConfig<FlagName, Tests> = AbbyConfig<FlagName, Tests>,
+>({
+  abbyConfig,
+}: {
+  abbyConfig: F.Narrow<AbbyConfig<FlagName, Tests>>;
+}) => {
+  const abbyNodeInstance = createAbby(abbyConfig);
+  const configNarrowed = abbyConfig as unknown as ConfigType;
+  /**
+   * hook to disbale a path via feature flag
+   */
+  const featureFlagHook = <F extends keyof Flags>(
+    key: F,
+    request: FastifyRequest,
+    reply: FastifyReply,
+    done: HookHandlerDoneFunction
+  ) => {
+    const flagValue = abbyNodeInstance.getFeatureFlagValue(key as unknown as FlagName); //TODO fix type
+    if (!flagValue) {
+      reply.status(403).send();
+      console.log("disbaled endpoint");
+      return;
+    }
+    done();
+  };
+  /**
+   * hook to parse all ab values on the request object needs to be used at the top
+   */
+  const ABTestHook = <T extends keyof Tests>(
+    key: T,
+    request: FastifyRequest,
+    reply: FastifyReply,
+    done: HookHandlerDoneFunction
+  ) => {};
 
-  const ABTestHook = () => {};
+  const getFlagValue = <F extends keyof Flags>(key: F) => {
+    return abbyNodeInstance.getFeatureFlagValue(key as unknown as FlagName);
+  };
 
-  const getFlagValue = () => {};
+  const getTestValue = <T extends keyof Tests>(key: T) => {
+    return abbyNodeInstance.getABTestValue(key);
+  };
 
-  const getTestValue = () => {};
-
-  return {};
+  return { ABTestHook, featureFlagHook, getFlagValue, getTestValue };
 };
--- packages/nodejs/src/fastify/index.ts
+++ packages/nodejs/src/fastify/index.ts
@@ -3,6 +3,7 @@ import { parseCookies } from "../shared/helpers";
 import { abby } from "./createAbby";
 import { setRequest, getRequest } from "../abby/contexts/requestContext";
 import { setResponse } from "../abby/contexts/responseContext";
+import { abbyFastifyFactory } from "./fastifyHookFactory";
 
 const fastify = Fastify();
 
@@ -10,6 +11,35 @@ fastify.register(require("@fastify/cookie"), {
   secret: "my-secret", // for cookies signature
   hook: "onRequest", // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
   parseOptions: {}, // options for parsing cookies
+});
+
+const { featureFlagHook } = abbyFastifyFactory({
+  abbyConfig: {
+    projectId: "clfn3hs1t0002kx08x3kidi80",
+    currentEnvironment: process.env.NODE_ENV,
+    tests: {
+      "New Test3": {
+        variants: ["A", "B"],
+      },
+      "New Test6": {
+        variants: ["A"],
+      },
+    },
+    flags: {
+      lol: "Boolean",
+      test3: "Boolean",
+      testAbby: "Boolean",
+    },
+    flagCacheConfig: {
+      refetchFlags: true,
+      timeToLive: 1,
+    },
+  },
+});
+
+fastify.addHook("onRequest", async (request, reply, done) => {
+  console.log("hook");
+  featureFlagHook("lol", request, reply, done);
 });
 
 const port = 3000;
