4cd564d5837b3c9769fe97c3c24238acb4d9d8c3
Tim Trost <82676248+Tim-53@users.noreply.github.com>
2023-07-19 15:40:55 +0200

    add marketing layout

--- apps/web/src/pages/contact.tsx
+++ apps/web/src/pages/contact.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { Navbar } from "../components/Navbar";
+import { MarketingLayout } from "../components/MarketingLayout";
 import { Footer } from "../components/Footer";
 import { useForm } from "react-hook-form";
 import { FormEventHandler } from "react";
@@ -38,83 +38,84 @@ export default function ContactPage() {
 
   return (
     <>
-      <div className="h-screen bg-primary-background text-primary-foreground">
-        <Navbar />
-        <main className=" mx-auto mt-5 flex h-full w-full min-w-min flex-col items-center justify-center  pb-40 sm:mt-5 sm:h-4/5">
-          <div className="mb-10  flex w-full flex-col  md:w-8/12 md:pb-24">
-            <div className="mx-auto mb-10 flex self-center">
-              <h2 className="mark self-center font-bold sm:text-xl md:text-4xl ">
-                Contact us
-              </h2>
-              <svg
-                className="h-14 w-16 sm:h-32 sm:w-32"
-                viewBox="0 0 120 90"
-                fill="none"
-              >
-                <path
-                  d="M59.2441 60.5857L59.6913 60.8093L60.1385 60.5857L118.383 31.4637V88.5369H1V31.4637L59.2441 60.5857ZM59.6913 43.6504L1 14.3048V1H118.383V14.3048L59.6913 43.6504Z"
-                  stroke="#F9A8D4"
-                  strokeWidth="2"
-                />
-              </svg>
+      <MarketingLayout>
+        <div className="h-screen bg-primary-background text-primary-foreground">
+          <main className=" mx-auto mt-5 flex h-full w-full min-w-min flex-col items-center justify-center  pb-40 sm:mt-5 sm:h-4/5">
+            <div className="mb-10  flex w-full flex-col  md:w-8/12 md:pb-24">
+              <div className="mx-auto mb-10 flex self-center">
+                <h2 className="mark self-center font-bold sm:text-xl md:text-4xl ">
+                  Contact us
+                </h2>
+                <svg
+                  className="h-14 w-16 sm:h-32 sm:w-32"
+                  viewBox="0 0 120 90"
+                  fill="none"
+                >
+                  <path
+                    d="M59.2441 60.5857L59.6913 60.8093L60.1385 60.5857L118.383 31.4637V88.5369H1V31.4637L59.2441 60.5857ZM59.6913 43.6504L1 14.3048V1H118.383V14.3048L59.6913 43.6504Z"
+                    stroke="#F9A8D4"
+                    strokeWidth="2"
+                  />
+                </svg>
+              </div>
+              <div className="mx-auto max-w-xs text-center">
+                Do you have any questions, comments or suggestions?
+                <div>Do not hesitate to contact us.</div>
+              </div>
             </div>
-            <div className="mx-auto max-w-xs text-center">
-              Do you have any questions, comments or suggestions?
-              <div>Do not hesitate to contact us.</div>
-            </div>
-          </div>
-          <form
-            className="mx-auto flex w-full max-w-sm flex-col items-start md:w-8/12"
-            onSubmit={onSubmit}
-          >
-            <div className="mb-4 flex w-full flex-grow flex-col">
-              <label className="mb-1">Surname</label>
+            <form
+              className="mx-auto flex w-full max-w-sm flex-col items-start md:w-8/12"
+              onSubmit={onSubmit}
+            >
+              <div className="mb-4 flex w-full flex-grow flex-col">
+                <label className="mb-1">Surname</label>
+                <input
+                  {...register("surname")}
+                  className={inputFieldStyle}
+                  placeholder="Surname"
+                ></input>
+              </div>
+              <div className=" 3 mb-4 flex w-full flex-grow flex-col ">
+                <label className="mb-1 ">Name</label>
+                <input
+                  {...register("name")}
+                  className={inputFieldStyle}
+                  placeholder="Name"
+                ></input>
+              </div>
+              <div className=" mb-4 flex w-full flex-grow flex-col">
+                <label className="mb-1" htmlFor="mail adress">
+                  Mail address
+                </label>
+                <input
+                  {...register("mailadress")}
+                  className={inputFieldStyle}
+                  placeholder="your@mailadress.com"
+                ></input>
+              </div>
+              <div className="mb-4 flex w-full flex-grow flex-col">
+                <label className="mb-1" htmlFor="your message">
+                  Your message
+                </label>
+                <textarea
+                  {...register("message")}
+                  className="border-width h-32 min-h-max w-full rounded border-2 border-solid border-accent-background  bg-inherit"
+                  placeholder="Your message text here"
+                ></textarea>
+              </div>
               <input
-                {...register("surname")}
-                className={inputFieldStyle}
-                placeholder="Surname"
-              ></input>
-            </div>
-            <div className=" 3 mb-4 flex w-full flex-grow flex-col ">
-              <label className="mb-1 ">Name</label>
-              <input
-                {...register("name")}
-                className={inputFieldStyle}
-                placeholder="Name"
-              ></input>
-            </div>
-            <div className=" mb-4 flex w-full flex-grow flex-col">
-              <label className="mb-1" htmlFor="mail adress">
-                Mail address
-              </label>
-              <input
-                {...register("mailadress")}
-                className={inputFieldStyle}
-                placeholder="your@mailadress.com"
-              ></input>
-            </div>
-            <div className="mb-4 flex w-full flex-grow flex-col">
-              <label className="mb-1" htmlFor="your message">
-                Your message
-              </label>
-              <textarea
-                {...register("message")}
-                className="border-width h-32 min-h-max w-full rounded border-2 border-solid border-accent-background  bg-inherit"
-                placeholder="Your message text here"
-              ></textarea>
-            </div>
-            <input
-              className="border-1 mb-2 w-24 self-center rounded-md border-accent-foreground bg-accent-background text-accent-foreground"
-              type="submit"
-            />
-            <span className="max-auto max-w-sm text-xs text-primary-foreground">
-              By submitting this form you agree to our{" "}
-              <a className="text-pink-300">Terms of use</a> and{" "}
-              <a className="text-pink-300">Privacy Policy</a>
-            </span>
-          </form>
-        </main>
-      </div>
+                className="border-1 mb-2 w-24 self-center rounded-md border-accent-foreground bg-accent-background text-accent-foreground"
+                type="submit"
+              />
+              <span className="max-auto max-w-sm text-xs text-primary-foreground">
+                By submitting this form you agree to our{" "}
+                <a className="text-pink-300">Terms of use</a> and{" "}
+                <a className="text-pink-300">Privacy Policy</a>
+              </span>
+            </form>
+          </main>
+        </div>
+      </MarketingLayout>
     </>
   );
 }
