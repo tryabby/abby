d2c6f610325bc019a33599fbbd9899747cda662f
Tim Raderschad <tim.raderschad@gmail.com>
2023-08-04 09:12:23 +0200

    Update apps/angular-example/src/app/app.module.ts
    Co-authored-by: slaesh <55546823+slaesh@users.noreply.github.com>

--- apps/angular-example/src/app/app.module.ts
+++ apps/angular-example/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { APP_INITIALIZER, Injectable, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import abbyConfig from "../../abby.config";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
