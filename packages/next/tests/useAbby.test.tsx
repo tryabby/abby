3cf38af4ef40cbe20dd1370f50b72d509fe3c1c3
Tim <tim.raderschad@gmail.com>
2023-08-14 11:58:40 +0200

    feat(next/react): fix lookup typings

--- packages/next/tests/useAbby.test.tsx
+++ packages/next/tests/useAbby.test.tsx
@@ -42,7 +42,7 @@ describe("useAbby", () => {
 
     const wrapper = ({ children }: PropsWithChildren) => <AbbyProvider>{children}</AbbyProvider>;
 
-    expectTypeOf(useAbby).parameters.toEqualTypeOf<["test" | "test2"]>();
+    expectTypeOf(useAbby).parameter(0).toEqualTypeOf<"test" | "test2">();
 
     const { result } = renderHook(() => useAbby("test"), {
       wrapper,
@@ -67,6 +67,7 @@ describe("useAbby", () => {
 
   it("uses lookup object when retrieving AB test variant", () => {
     const { getABTestValue } = createAbby({
+      environments: [],
       projectId: "123",
       tests: {
         test2: {
@@ -86,5 +87,54 @@ describe("useAbby", () => {
     const value = getABTestValue("test2", undefined, lookupMap)[0];
 
     expect(value).toBe(lookupMap[activeVariant]);
+  });
+
+  it("produces proper types with a lookup objects", () => {
+    const { getABTestValue } = createAbby({
+      environments: [],
+      projectId: "123",
+      currentEnvironment: "a",
+      tests: {
+        test: {
+          variants: ["A", "B", "C"],
+        },
+      },
+    });
+
+    const [activeVariant] = getABTestValue("test", undefined, {
+      A: "Hello",
+      B: "Bonjour",
+      C: "Hola",
+    });
+
+    expectTypeOf(activeVariant).toEqualTypeOf<"Hello" | "Bonjour" | "Hola">();
+  });
+
+  it("produces proper types with a a lookup object in the hook", () => {
+    const { AbbyProvider, useAbby } = createAbby({
+      environments: [],
+      projectId: "123",
+      tests: {
+        test: {
+          variants: ["A", "B", "C"],
+        },
+      },
+    });
+
+    const wrapper = ({ children }: PropsWithChildren) => <AbbyProvider>{children}</AbbyProvider>;
+
+    const { result } = renderHook(
+      () =>
+        useAbby("test", {
+          A: "Hello",
+          B: "Bonjour",
+          C: "Hola",
+        }),
+      {
+        wrapper,
+      }
+    );
+
+    expectTypeOf(result.current.variant).toEqualTypeOf<"Hello" | "Bonjour" | "Hola">();
   });
 });
