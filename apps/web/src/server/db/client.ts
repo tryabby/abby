e63fab51111bd60985b4e457c36f6bb334b7b636
Tim Raderschad <tim.raderschad@gmail.com>
2023-10-11 20:20:35 +0200

    feat(web): add planetscale db driver for prisma (#83)

--- apps/web/src/server/db/client.ts
+++ apps/web/src/server/db/client.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { connect } from "@planetscale/database";
+import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 
 import { env } from "../../env/server.mjs";
 
@@ -9,9 +11,16 @@ declare global {
 
 export const prisma =
   global.prisma ||
-  new PrismaClient({
-    log: ["error"],
-  });
+  // enable planetscale adapter in production if enabled
+  (env.NODE_ENV === "production" && env.USE_PLANETSCALE
+    ? (() => {
+        const connection = connect({ url: env.DATABASE_URL });
+        const adapter = new PrismaPlanetScale(connection);
+        return new PrismaClient({ adapter, log: ["error"] });
+      })()
+    : new PrismaClient({
+        log: ["error"],
+      }));
 
 if (env.NODE_ENV !== "production") {
   global.prisma = prisma;
