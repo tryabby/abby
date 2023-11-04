3a0b3529fa6629831460d4d60558ea4aa959e46d
Tim Raderschad <tim.raderschad@gmail.com>
2023-11-04 21:44:11 +0000

    fix plausible build

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type AppType } from "next/app";
-import { type Session } from "next-auth";
 import PlausibleProvider from "next-plausible";
-const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps }) => {
+
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <PlausibleProvider domain="docs.tryabby.com">
       <Component {...pageProps} />
