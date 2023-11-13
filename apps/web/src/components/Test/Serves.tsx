390343b515ac535395285a3ab021c4f4d38039f6
Tim Raderschad <tim.raderschad@gmail.com>
2023-11-13 08:10:29 +0100

    wip

--- apps/web/src/components/Test/Serves.tsx
+++ apps/web/src/components/Test/Serves.tsx
@@ -1,60 +1,9 @@
 import { TestConversion } from "@prisma/client";
-import {
-  Chart as ChartJS,
-  BarElement,
-  CategoryScale,
-  Legend,
-  LinearScale,
-  Title,
-  Tooltip,
-  ChartOptions,
-} from "chart.js";
+import { Card, DeltaBar, DonutChart, Text, Title } from "@tremor/react";
+
 import { useMemo } from "react";
 import { Bar } from "react-chartjs-2";
 import type { ClientOption } from "server/trpc/router/project";
-
-ChartJS.defaults.font.family = "Mona Sans";
-ChartJS.defaults.color = "white";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-export const OPTIONS: ChartOptions<"bar"> = {
-  responsive: true,
-  maintainAspectRatio: false,
-  scales: {
-    y: {
-      min: 0,
-      max: 100,
-    },
-  },
-  plugins: {
-    legend: {
-      position: "top" as const,
-    },
-    tooltip: {
-      callbacks: {
-        label: function (context) {
-          let label = context.dataset.label || "";
-
-          if (label) {
-            label += ": ";
-          }
-          if (context.parsed.y !== null) {
-            label += context.parsed.y;
-          }
-          return `${label}%`;
-        },
-      },
-    },
-  },
-};
 
 const Serves = ({
   pingEvents,
@@ -63,45 +12,48 @@ const Serves = ({
   pingEvents: TestConversion[];
   options: ClientOption[];
 }) => {
-  const labels = options.map((option) => option.identifier);
-
   const actualData = useMemo(() => {
     return options.map((option) => {
-      return pingEvents.filter(
-        (event) => event.selectedVariant === option.identifier
-      ).length;
+      return {
+        name: option.identifier,
+        count: pingEvents.filter(
+          (event) => event.selectedVariant === option.identifier
+        ).length,
+      };
     });
   }, [options, pingEvents]);
 
   const absPings = actualData.reduce((accumulator, value) => {
-    return accumulator + value;
+    return accumulator + value.count;
   }, 0);
 
+  const expectedData = useMemo(() => {
+    return options.map((option) => ({
+      name: option.identifier,
+      count: absPings * option.chance,
+    }));
+  }, [absPings, options]);
+
   return (
-    <div className="relative h-full w-full">
-      <Bar
-        className="self-end"
-        options={OPTIONS}
-        data={{
-          labels,
-          datasets: [
-            {
-              label: "Target",
-              data: options.map(
-                (option) => parseFloat(option.chance.toString()) * 100
-              ),
-              backgroundColor: "#A9E4EF",
-            },
-            {
-              label: "Actual",
-              data: actualData.map((data) =>
-                Math.round((data / absPings) * 100)
-              ),
-              backgroundColor: "#f472b6",
-            },
-          ],
-        }}
-      />
+    <div className="relative grid h-full w-full gap-y-4 md:grid-cols-2">
+      <div className="flex flex-col items-center space-y-3">
+        <Text>Views</Text>
+        <DonutChart
+          data={actualData}
+          category="count"
+          index="name"
+          colors={["pink", "indigo"]}
+        />
+      </div>
+      <div className="flex flex-col items-center space-y-3">
+        <Text>Expected</Text>
+        <DonutChart
+          data={expectedData}
+          category="count"
+          index="name"
+          colors={["pink", "indigo"]}
+        />
+      </div>
     </div>
   );
 };
