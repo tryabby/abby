cc43be2954fbae2fd7bb2a74219c4a0b6a38fdcf
Tim Raderschad <tim.raderschad@gmail.com>
2023-11-15 11:16:02 +0100

    feat(web): use planetscale connection pooling (#100)

--- apps/web/src/server/db/client.ts
+++ apps/web/src/server/db/client.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { connect } from "@planetscale/database";
+import { createRequire } from "node:module";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
+
+// hotfix for planetscale adapter (https://github.com/prisma/prisma/issues/21956)
+const require = createRequire(import.meta.url);
+const { Client } = require("@planetscale/database");
 
 import { env } from "../../env/server.mjs";
 
@@ -12,10 +16,10 @@ declare global {
 export const prisma =
   global.prisma ||
   // enable planetscale adapter in production if enabled
-  (env.NODE_ENV === "production" && env.USE_PLANETSCALE === "true"
+  (env.USE_PLANETSCALE === "true"
     ? (() => {
-        const connection = connect({ url: env.DATABASE_URL });
-        const adapter = new PrismaPlanetScale(connection);
+        const client = new Client({ url: env.DATABASE_URL });
+        const adapter = new PrismaPlanetScale(client);
         return new PrismaClient({ adapter, log: ["error"] });
       })()
     : new PrismaClient({
