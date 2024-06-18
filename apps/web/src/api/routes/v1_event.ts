ecf17c0680c515360269b0a1816ce4b3b35a2b30
Rajesh Jonnalagadda <38752904+rajeshj11@users.noreply.github.com>
2024-06-18 15:33:42 +0530

    fix:removed the upstash redis and upstash ratelimit (#144)
    * fix:removed the upstash redis and upstash ratelimit
    
    * revert back to 10 requests in 10 secs
    
    * refactor:moved the rate lmiting funcitons to utils
    
    * refactor:moved te rate limit utils  to server/common and rename the file
    
    * chore:updated the pnpm lock file
    
    * chore: use proper pnpm lockfile version
    
    ---------
    
    Co-authored-by: Tim Raderschad <tim.raderschad@gmail.com>

--- apps/web/src/api/routes/v1_event.ts
+++ apps/web/src/api/routes/v1_event.ts
@@ -1,13 +1,12 @@
 import { zValidator } from "@hono/zod-validator";
 import { abbyEventSchema, AbbyEventType } from "@tryabby/core";
-import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
 import { Hono } from "hono";
 
 import isbot from "isbot";
 import { EventService } from "server/services/EventService";
 import { RequestCache } from "server/services/RequestCache";
 import { RequestService } from "server/services/RequestService";
+import { checkRateLimit } from "server/common/ratelimit";
 
 export function makeEventRoute() {
   const app = new Hono().post(
@@ -26,11 +25,6 @@ export function makeEventRoute() {
         //   // we only want to rate limit in production
         if (process.env.NODE_ENV === "production") {
           // Create a new ratelimiter, that allows 10 requests per 10 seconds
-          const ratelimit = new Ratelimit({
-            redis: Redis.fromEnv(),
-            limiter: Ratelimit.slidingWindow(10, "10 s"),
-          });
-
           const clientIp = (c.req.header("x-forwarded-for") || "")
             .split(",")
             .pop()
@@ -42,7 +36,7 @@ export function makeEventRoute() {
             return c.text("Internal Server Error", 500);
           }
 
-          const { success } = await ratelimit.limit(clientIp);
+          const success = await checkRateLimit(clientIp);
 
           if (!success) {
             return c.text("Too Many Requests", 429);
---
+++ apps/web/src/server/common/ratelimit.ts
@@ -0,0 +1,18 @@
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import { redis } from "server/db/redis";
+
+export const rateLimiter = new RateLimiterRedis({
+  storeClient: redis,
+  keyPrefix: "rateLimiter",
+  points: 10, // Number of points
+  duration: 10, // Per 10 seconds
+});
+
+export const checkRateLimit = async (ip: string) => {
+  try {
+    await rateLimiter.consume(ip);
+    return true;
+  } catch (rateLimiterRes) {
+    return false;
+  }
+};
