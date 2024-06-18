cd27cc56824b5c95e7e570fbfe8b0e9cce46a760
Rajesh Jonnalagadda <rajeshjonnalagadda48@gmail.com>
2024-06-18 13:50:37 +0530

    refactor:moved te rate limit utils  to server/common and rename the file

--- apps/web/src/api/routes/v1_event.ts
+++ apps/web/src/api/routes/v1_event.ts
@@ -6,7 +6,7 @@ import isbot from "isbot";
 import { EventService } from "server/services/EventService";
 import { RequestCache } from "server/services/RequestCache";
 import { RequestService } from "server/services/RequestService";
-import { checkRateLimit } from "./utils";
+import { checkRateLimit } from "server/common/ratelimit";
 
 export function makeEventRoute() {
   const app = new Hono().post(
