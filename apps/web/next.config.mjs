a6c22bea9792ceb28fd748592f3d57e5200c691a
Tim Raderschad <tim.raderschad@gmail.com>
2024-08-03 02:50:03 +0200

    fix(web): loosen security headers

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -32,9 +32,7 @@ const withBundleAnalyzer = bundleAnalzyer({
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
